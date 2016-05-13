2b0449da9c569f8a7cda8f9e6c430512860d073c
Anna Natorilla <annagaiel@gmail.com>
2016-05-12 19:44:58 -0700

    move the show method

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
   gem 'dotenv-rails', '~> 2.1'
+  gem 'pry'
 
 end
 
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,24 +1,19 @@
 class JobsController < ApplicationController
-  before_action :set_url, only: [:index, :show, :create]
-
-  def show
-    @job = Unirest.get(@url+"/#{params[:id]}.json").body
-  end
 
   def index
-    @jobs = Unirest.get(@url+".json").body
+    @jobs = Unirest.get("#{ENV['DOMAIN']}/jobs.json").body
   end
 
   def new
   end
 
+  def show
+    @job = Unirest.get("#{ENV['DOMAIN']}/jobs/#{params[:id]}.json").body
+  end
+  
   def create
-    @job = Unirest.post(@url+"?title=#{params[:title]}&description=#{params[:description]}&salary=#{params[:salary]}")
+    @job = Unirest.post("#{ENV['DOMAIN']}/jobs?title=#{params[:title]}&description=#{params[:description]}&salary=#{params[:salary]}")
     render "http://localhost:3001/jobs/"
   end
 
-  private
-  def set_url
-    @url = "http://localhost:3000/api/v1/jobs"
-  end
 end
